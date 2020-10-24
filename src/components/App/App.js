5a399f9f3da5010fc81fb9cdf3396212c0b0e2bc
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-24 18:46:28 +0300

    Add react-router-dom & create header.css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 function App() {
   return (
     <div className="page">
-      <header className="App-header">
-        <h1>Hello!</h1>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header className="header">
+      <h3 className="header__text">NewsExplorer</h3>
+      <ul className="header__list">
+        <li className="header__links">
+          <Link className="header__link">Главная</Link>
+        </li>
+        <li className="header__links">
+          <Link className="header__link">Сохранённые статьи</Link>
+        </li>
+        <li className="header__links header__links_logged">
+          <Link className="header__link">Авторизоваться</Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
