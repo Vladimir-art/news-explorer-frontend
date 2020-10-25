17d58c46dc80aadc61486d6241e39b0c91373606
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-25 13:35:26 +0300

    searchform__text style

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,7 +5,7 @@ function SearchForm() {
     <section className="searchform">
       <div className="searchform__info">
         <h1 className="searchform__title">Что творится в мире?</h1>
-        <p className="searchform__text">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.</p>
+        <p className="searchform__text">Находите самые свежие статьи на любую тему и сохраняйте в&nbsp;своём личном кабинете.</p>
         <form className="searchform__form">
           <input className="searchform__input" type="text" placeholder="Введите тему новости"></input>
           <button className="searchform__search" type="submit">Искать</button>
