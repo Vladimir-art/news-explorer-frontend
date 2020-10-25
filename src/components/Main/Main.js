c439065eaa2f13669e76307bbb068add43ae805f
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-25 17:02:07 +0300

    Add PafeNotFound

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import SearchForm from '../SearchForm/SearchForm';
 import Preloader from '../Preloader/Preloader';
+import PageNotFound from '../PageNotFound/PageNotFound';
 
 function Main() {
   return (
     <main className="main">
       <SearchForm />
       <Preloader />
+      <PageNotFound />
     </main>
   );
 }
