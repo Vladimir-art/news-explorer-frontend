5ca201be8fb3e0082872a0fc662ef99a4cf99cfb
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-25 19:56:05 +0300

    NewsCard markup

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 function NewsCard() {
   return (
-    <>
-    </>
+    <div className="article-element">
+      <img className="article-element__image" alt="Изображение" src="#" />
+      <p className="article-element__date"></p>
+      <h3 className="article-element__title"></h3>
+      <p className="article-element__text"></p>
+      <p className="article-element__source"></p>
+    </div>
   );
 }
 
