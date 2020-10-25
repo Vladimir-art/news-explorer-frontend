ba76e882faee7cda42062af14d784d88aef8f9de
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-25 19:48:17 +0300

    create NewsCard & add it

--- src/components/SearchingResults/SearchingResults.js
+++ src/components/SearchingResults/SearchingResults.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import NewsCard from '../NewsCard/NewsCard';
 
 function SearchingResults() {
   return (
     <section className="searching-results">
       <h2 className="searching-results__title">Результаты поиска</h2>
-      <div className="searching-results__elements"></div>
+      <div className="searching-results__elements">
+        <NewsCard />
+      </div>
       <button className="searching-results__button" type="button">Показать еще</button>
     </section>
   );
