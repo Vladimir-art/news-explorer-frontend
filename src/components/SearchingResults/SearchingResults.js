6a30942014b08257731f6fc59ace17fbc9e45a15
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-25 19:19:53 +0300

    searching-results markup

--- src/components/SearchingResults/SearchingResults.js
+++ src/components/SearchingResults/SearchingResults.js
@@ -3,7 +3,9 @@ import React from 'react';
 function SearchingResults() {
   return (
     <section className="searching-results">
-
+      <h2 className="searching-results__title">Результаты поиска</h2>
+      <div className="searching-results__elements"></div>
+      <button className="searching-results__button">Показать еще</button>
     </section>
   );
 }
