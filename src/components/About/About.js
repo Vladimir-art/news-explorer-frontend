abb7dc758303eda69b3d7c93a82aaf15c89f696d
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-26 21:49:36 +0300

    about__knowlege styles

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,12 +7,12 @@ function About() {
       <div className="about__cover">
         <img className="about__image" alt="Автор" src={author} />
         <div className="about__info">
-        <h2 className="about__title">Об авторе</h2>
+          <h2 className="about__title">Об авторе</h2>
           <p className="about__knowledge">
             Это блок с описанием автора проекта. Здесь следует указать,
             как вас зовут, чем вы занимаетесь, какими технологиями разработки владеете.
           </p>
-          <p className="about__author">Также можно рассказать о процессе обучения в Практикуме, чему вы тут научились, и чем можете помочь потенциальным заказчикам.</p>
+          <p className="about__knowledge">Также можно рассказать о процессе обучения в Практикуме, чему вы тут научились, и чем можете помочь потенциальным заказчикам.</p>
         </div>
       </div>
     </section>
