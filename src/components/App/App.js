d8159510a33ae3f6790d01c976eec0ed0a7bc7cc
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-26 21:54:15 +0300

    Create Footer & add it to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <div className="page">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
