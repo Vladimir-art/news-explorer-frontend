f5aa14f6e3031f62b8aec485809d45f31562b22a
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-27 21:38:46 +0300

    Create Register

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
+import Register from '../Register/Register';
 import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <div className="page">
       <Header />
+      <Register />
       <Main />
       <Footer />
     </div>
