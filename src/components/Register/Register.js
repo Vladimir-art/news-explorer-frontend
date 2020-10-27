611476d384291bc680ba7ec2a97d53d3ab2f113c
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-27 22:44:12 +0300

    register-container__name

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -9,18 +9,19 @@ function Register() {
           <label className="register-container__name" id="email">
             Email
             <input className="register-container__input" id="email" name="email" placeholder="Введите почту" type="email" />
-            <span className="register-container__input-error" id="email-error"></span>
+            <span className="register-container__input-error" id="email-error">Неправильный формат</span>
           </label>
           <label className="register-container__name" id="password">
             Пароль
             <input className="register-container__input" id="password" name="password" placeholder="Введите пароль" type="password" />
-            <span className="register-container__input-error" id="password-error"></span>
+            <span className="register-container__input-error" id="password-error">Неправильный формат</span>
           </label>
           <label className="register-container__name" id="name">
             Имя
             <input className="register-container__input" id="name" name="name" placeholder="Введите своё имя" type="text" pattern="[A-Za-zАЯ-Ёая-ё -]{1,}" />
-            <span className="register-container__input-error" id="name-error"></span>
+            <span className="register-container__input-error" id="name-error">Неправильный формат</span>
           </label>
+          <span className="register-container__input-error" id="name-error">Такой пользователь уже есть</span>
           <button className="register-container__button" type="submit">Зарегистрироваться</button>
           <p className="register-container__choice">или <a className="register-container__link" href="#">Войти</a></p>
         </fieldset>
