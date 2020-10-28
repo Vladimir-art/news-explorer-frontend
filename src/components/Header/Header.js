d6192f34294793c641332688e87c715ed4b2c03f
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-28 22:29:51 +0300

    Add theme dark to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <header className="header">
       <div className="header__info">
         <div className="header__mobile">
-          <h3 className="header__text">NewsExplorer</h3>
+          <h3 className="header__text header__text_theme-dark">NewsExplorer</h3>
           <button className="header__button"></button>
         </div>
         <Navigation />
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,17 +5,17 @@ import logOut from '../../images/logout.svg';
 function Navigation() {
   return (
     <ul className="navigation">
-      <li className="navigation__links navigation__links_hover">
-        <Link className="navigation__link">Главная</Link>
+      <li className="navigation__links navigation__links_hover navigation__links_hover_theme-dark">
+        <Link className="navigation__link navigation__link_theme-dark">Главная</Link>
       </li>
-      <li className="navigation__links navigation__links_hover">
-        <Link className="navigation__link">Сохранённые статьи</Link>
+      <li className="navigation__links navigation__links_hover navigation__links_hover_theme-dark">
+        <Link className="navigation__link navigation__link_theme-dark">Сохранённые статьи</Link>
       </li>
-      <li className="navigation__links navigation__links_loggedout">
-        <Link className="navigation__link">Авторизоваться</Link>
+      <li className="navigation__links navigation__links_loggedout navigation__links_theme-dark">
+        <Link className="navigation__link navigation__link_theme-dark">Авторизоваться</Link>
       </li>
-      <li className="navigation__links navigation__links_loggedin">
-        <Link className="navigation__link">Имя</Link>
+      <li className="navigation__links navigation__links_loggedin navigation__links_theme-dark">
+        <Link className="navigation__link navigation__link_theme-dark">Имя</Link>
         <img className="navigation__image" alt="Иконка-выйти" src={logOut} />
       </li>
     </ul>
