be711afa684c0464cdb380dbf15ac5d221fd0ba4
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-31 12:31:09 +0300

    Css folder NewsCardList

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 function NewsCardList() {
   return (
-    <>
-    </>
+    <section className="article-list">
+      <div className="article-list__item">
+
+      </div>
+    </section>
   );
 }
 
