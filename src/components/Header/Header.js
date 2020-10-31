96937b5578545205bbcabe5cf2e35ba79a87070e
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-31 19:54:53 +0300

    .header 768px

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,7 @@ function Header() {
   return (
     <header className="header">
       <div className="header__info">
-        <div className="header__mobile">
-          <h3 className="header__text">NewsExplorer</h3>
-          <button className="header__button"></button>
-        </div>
+        <h3 className="header__text">NewsExplorer</h3>
         <Navigation />
       </div>
     </header>
