4bcdfd3f3c8432e1c50f6d8d12ba4f5f0529f4a9
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-11-01 17:02:39 +0300

    saved-articles 320

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,10 +3,10 @@ import Navigation from '../Navigation/Navigation';
 
 function Header() {
   return (
-    <header className="header header__main-background ">
+    <header className="header header__main-background header__main-background_inactive">
       <div className="header__info">
         <div className="header__mobile">
-          <h3 className="header__text ">NewsExplorer</h3>
+          <h3 className="header__text header__text_theme-dark">NewsExplorer</h3>
           <button className="header__button header__button_active"></button>
         </div>
         <div className="header__nav">
