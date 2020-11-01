fcd4b2012c1545d87fa8fd16a1bf68ab75514b1d
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-11-01 21:07:14 +0300

    add function to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,16 +3,20 @@ import Navigation from '../Navigation/Navigation';
 
 function Header() {
 
+  const [activeMenu, setActiveMenu] = React.useState(false);
+
   function changeButton(e) {
     if (e.target.classList.contains('header__button')) {
-      e.target.classList.toggle('header__button_active')
+      e.target.classList.toggle('header__button_active');
+      setActiveMenu(!activeMenu);
     } else {
-      e.target.querySelector('.header__button').classList.toggle('header__button_active')
+      e.target.querySelector('.header__button').classList.toggle('header__button_active');
+      setActiveMenu(!activeMenu);
     }
   }
 
   return (
-    <header className="header header__main-background header__main-background_mobile">
+    <header className={`header header__main-background ${activeMenu && `header__main-background_mobile`}`}>
       <div className="header__info">
         <div className="header__mobile">
           <h3 className="header__text ">NewsExplorer</h3>
@@ -20,7 +24,7 @@ function Header() {
             <div className="header__button "></div>
           </button>
         </div>
-        <div className="header__nav ">
+        <div className={`header__nav ${activeMenu && `header__nav_active`}`}>
           <Navigation />
         </div>
       </div>
