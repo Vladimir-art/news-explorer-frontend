5066937605b73f370cbc16af5072b399cfeec675
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-11-01 19:37:12 +0300

    transition icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,23 @@ import React from 'react';
 import Navigation from '../Navigation/Navigation';
 
 function Header() {
+
+  function changeButton(e) {
+    if (e.target.classList.contains('header__button')) {
+      e.target.classList.toggle('header__button_active')
+    } else {
+      e.target.querySelector('.header__button').classList.toggle('header__button_active')
+    }
+  }
+
   return (
     <header className="header header__main-background header__main-background_mobile">
       <div className="header__info">
         <div className="header__mobile">
           <h3 className="header__text ">NewsExplorer</h3>
-          <div className="header__cover">
-            <button className="header__button header__button_active"></button>
-          </div>
+          <button className="header__cover" onClick={changeButton}>
+            <div className="header__button "></div>
+          </button>
         </div>
         <div className="header__nav">
           <Navigation />
