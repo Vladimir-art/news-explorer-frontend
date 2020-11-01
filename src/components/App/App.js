d298ebf6370b8d57cc80ec17d31dc30b2873aff4
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-11-01 18:49:50 +0300

    header__main-background_mobile

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 // import SavedNews from '../SavedNews/SavedNews';
-import Register from '../Register/Register';
+// import Register from '../Register/Register';
 import Footer from '../Footer/Footer';
 // import Login from '../Login/Login';
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="page">
       <Header />
-      <Register />
+      {/* <Register /> */}
       {/* <Login /> */}
       <Main />
       {/* <SavedNews /> */}
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import Navigation from '../Navigation/Navigation';
 
 function Header() {
   return (
-    <header className="header header__main-background ">
+    <header className="header header__main-background header__main-background_mobile">
       <div className="header__info">
         <div className="header__mobile">
           <h3 className="header__text ">NewsExplorer</h3>
