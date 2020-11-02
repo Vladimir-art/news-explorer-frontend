07920ae288d499a6526faf3cddc77dbfd3b7db56
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-11-02 20:47:58 +0300

    Create Route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import Header from '../Header/Header';
 import Main from '../Main/Main';
-// import SavedNews from '../SavedNews/SavedNews';
+import SavedNews from '../SavedNews/SavedNews';
 // import Register from '../Register/Register';
 import Footer from '../Footer/Footer';
 // import Login from '../Login/Login';
 
 function App() {
+
+  // const [changeTheme, setChangeTheme] = React.useState(false);
+
+  // function changeTheme() {
+  //   setChangeTheme
+  // }
+
   return (
     <div className="page">
       <Header />
       {/* <Register /> */}
       {/* <Login /> */}
-      <Main />
-      {/* <SavedNews /> */}
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route path="/saved-news">
+          <SavedNews />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
