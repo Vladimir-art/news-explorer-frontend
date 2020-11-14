9e89672b56ce1ca33da2c73cbb524c434283920e
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-11-14 18:47:13 +0300

    ProtectedRoute скуфеу

---
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const ProtectedRoute = ({ component: Component, ...props }) => {
+  return (
+    <Route>
+      {
+        () => props.loggedIn ? <Component {...props} /> : <Redirect to="./" />
+      }
+    </Route>
+  )
+}
+
+export default ProtectedRoute;
